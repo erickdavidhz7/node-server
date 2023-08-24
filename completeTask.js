90ba832d28495786d5b222861424bbafebba37ea
Erick Henriquez <erickdavid7@hotmail.com>
2023-08-24 12:02:10 -0500

    pequeñas mejoras del output

--- completeTask.js
+++ completeTask.js
@@ -1,15 +1,17 @@
 const readlineSync = require("readline-sync");
 
-function completeTask(taskList){
-    let idInput = parseInt(readlineSync.question("Please enter the id of the task that you would like complete: "), 10);
-    for (const task of taskList) {
-        if (task.id === idInput){
-            task.status = "Completed"
-            console.log(`The task of id: ${idInput}\nHas been assigned as completed.`);
-            return;
-        }
+function completeTask(taskList) {
+  let idInput = parseInt(readlineSync.question("Please enter the id of the task that you would like complete: "),10);
+  for (const task of taskList) {
+    if (task.id === idInput) {
+      task.status = "Completed";
+      console.log(
+        `The task of id: ${idInput}\nHas been assigned as completed.`
+      );
+      return;
     }
-    console.log("The id that you have entered was not found");
+  }
+  console.log("The id that you have entered was not found");
 }
 
 module.exports = completeTask;
--- displayTasks.js
+++ displayTasks.js
@@ -1,7 +1,8 @@
 function displayTasks(taskList) {
-  console.log("---------- Printing the tasks----------- ");
+  console.log("------------ Printing tasks------------- ");
   taskList.map((task, taskNumber) => {
     console.log(`Task ${taskNumber + 1}`);
+    console.log(`ID: ${task.id}`);
     console.log(`Name: ${task.name}`);
     console.log(`Description: ${task.description}`);
     console.log(`Status: ${task.status}`);
